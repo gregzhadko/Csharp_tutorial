0dd6e76c3e4fdf04ffcb28820d127054a5d25b75
Grigory Zhadko <gregzhadko@gmail.com>
2017-07-06 17:18:01 +0200

    Hello World

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");  //Выводит строку в консоль
+            Console.ReadLine();                 //Ждёт когда пользователь введет в консоль строку и нажмет Enter. Сейчас это нужно, чтобы консоль просто не закрывалась.
+            
+            //А это просто комментарий.
+            /* И это тоже комментарий.
+            */
+        }
+    }
+}
