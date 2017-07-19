6a98056b105ec29b9a2d162634a167b10dec84ac
Grigory Zhadko <gregzhadko@gmail.com>
2017-07-19 18:51:21 +0200

    Added empty lesson6

---
+++ Lesson6/Lesson6.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson6
+{
+    internal class Lesson6
+    {
+        public static void Main(string[] args)
+        {
+            //Вчера я допустил 
+            //Посмотрим на еще две полезные фичи при работе с циклами
+            //
+        }
+    }
+}
