653990f12acf9f942e639716a41091d54479e135
Grigory Zhadko <gregzhadko@gmail.com>
2017-07-25 00:20:23 +0200

    Added empty lesson 8

---
+++ Lesson8/Lesson8.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson8
+{
+    internal class Lesson8
+    {
+        public static void Main(string[] args)
+        {
+            //Рассмотрим условный оператор switch
+        }
+    }
+}
