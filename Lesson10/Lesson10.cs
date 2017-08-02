7aba4a08bcdbccf53528137c7e9dd763bc3e3ef8
Grigory Zhadko <gregzhadko@gmail.com>
2017-08-02 22:47:37 +0200

    Lesson 10 is added

---
+++ Lesson10/Lesson10.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson10
+{
+    internal class Lesson10
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        static int[] Multiply(int scalar, int[] vector)
+        {
+            return new int[0];
+        }
+
+        static int[] Multiply(int[] vector1, int[] vector2)
+        {
+            return new int[0];
+        }
+        
+        static int[,] Multiply(int[] vector, int[,] matrix)
+        {
+            return new int[0,0];
+        }
+        
+        static int[,] Multiply(int scalar, int[,] matrix)
+        {
+            return new int[0,0];
+        }
+        
+        static int[,] Multiply(int[,] matrix1, int[,] matrix2)
+        {
+            return new int[0,0];
+        }
+        
+        static int[,] Transform(int[,] matrix)
+        {
+            return new int[0,0];
+        }
+    }
+}
